a5469798bf2fd3ce13fcfb679d90cd8f33303319
YolandaKok <yolanda.kokkinou@gmail.com>
2020-01-09 16:09:55 +0200

    heroku test4

---
+++ src/main/java/com/or/tools/util/WebMvcConfiguration.java
@@ -0,0 +1,23 @@
+package com.or.tools.util;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+@Configuration
+public class WebMvcConfiguration implements ErrorViewResolver {
+
+	@Override
+	public ModelAndView resolveErrorView(HttpServletRequest request, HttpStatus status, Map<String, Object> model) {
+		if (status == HttpStatus.NOT_FOUND) {
+			return new ModelAndView("forward:/");
+		}
+		return null;
+	}
+
+}
