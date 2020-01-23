c028c962dca4a3326e436652c361ff0bb4eed3c7
YolandaKok <yolanda.kokkinou@gmail.com>
2020-01-23 21:01:56 +0200

    heroku test

--- src/main/webapp/src/components/algorithms/Knapsack.jsx
+++ src/main/webapp/src/components/algorithms/Knapsack.jsx
@@ -40,7 +40,9 @@ export class Knapsack extends Component {
                 {x: 360, y: 7, size: 2, color: 0.3},
                 {x: 83, y: 0, size: 2, color: 0.3},
                 {x: 59, y: 30, size: 2, color: 1.4},
-                {x: 130, y: 22, size: 2, color: 1.4}]
+                {x: 130, y: 22, size: 2, color: 1.4},
+                {x: 431, y: 80, size: 2, color: 1.4}
+            ]
         };
         this.onButtonClick = this.onButtonClick.bind(this);
     }
