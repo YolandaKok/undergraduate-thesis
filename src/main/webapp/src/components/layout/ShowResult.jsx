d7ddeb5ea657a38ed1de79cf369b05f7fa62379b
YolandaKok <yolanda.kokkinou@gmail.com>
2020-02-06 18:50:37 +0200

    Show Result table

--- src/main/webapp/src/components/layout/ShowResult.jsx
+++ src/main/webapp/src/components/layout/ShowResult.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import CustomTable from "./InstructionsPanel";
+import CustomTable from "../layout/CustomTable";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import styles from "../../static/signup.module.css";
@@ -12,7 +12,9 @@ class ShowResult extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            results: []
+            results: [],
+            totalValue: '',
+            totalWeight: ''
         }
     }
     componentDidMount() {
@@ -24,6 +26,8 @@ class ShowResult extends Component {
             console.log(response);
             let resultObj = JSON.parse(response.data.resultData);
             this.setState({"results": resultObj.points});
+            this.setState({"totalValue": resultObj.totalValue});
+            this.setState({"totalWeight": resultObj.totalWeight});
         },
         (error) => {
             console.log("error");
@@ -41,6 +45,11 @@ class ShowResult extends Component {
                     </Grid>
                     <Grid item xs={12} md={12} lg={6} xl={6}>
                         <CustomGraph data={this.state.results} titleX={'Values'} titleY={'Weights'}/>
+                    </Grid>
+                    <Grid item xs={12} md={12} lg={6} xl={6}>
+                        <CustomTable rows={this.state.results} checkResult={true}
+                                     totalValue={this.state.totalValue}
+                                     totalWeight={this.state.totalWeight}/>
                     </Grid>
                 </Grid>
             </Container>
