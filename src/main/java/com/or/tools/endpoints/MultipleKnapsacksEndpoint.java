b803c192277758f68188acef17c11f9dcb99169b
YolandaKok <yolanda.kokkinou@gmail.com>
2020-02-13 11:58:05 +0200

    multiple knapsacks

---
+++ src/main/java/com/or/tools/endpoints/MultipleKnapsacksEndpoint.java
@@ -0,0 +1,10 @@
+package com.or.tools.endpoints;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/multipleKnapsacks")
+public class MultipleKnapsacksEndpoint {
+
+}
