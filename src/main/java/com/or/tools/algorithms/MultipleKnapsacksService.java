d3ab442220a5d34aeb8ec102b0ed1e9cfade090a
YolandaKok <yolanda.kokkinou@gmail.com>
2020-02-27 15:25:19 +0200

    Refactoring in tsp

--- src/main/java/com/or/tools/algorithms/MultipleKnapsacksService.java
+++ src/main/java/com/or/tools/algorithms/MultipleKnapsacksService.java
@@ -1,5 +1,144 @@
 package com.or.tools.algorithms;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.google.ortools.linearsolver.MPConstraint;
+import com.google.ortools.linearsolver.MPObjective;
+import com.google.ortools.linearsolver.MPSolver;
+import com.google.ortools.linearsolver.MPVariable;
+import com.or.tools.model.BinWrapper;
+import com.or.tools.model.MultipleKnapsackDataRead;
+import com.or.tools.model.MultipleKnapsackDisplayResponse;
+import com.or.tools.model.MultipleKnapsackModel;
+import com.or.tools.model.MultipleKnapsackResponse;
+import com.or.tools.util.IOUtils;
+
 public class MultipleKnapsacksService {
 
+	@Autowired
+	private IOUtils utils;
+
+	public MultipleKnapsackDataRead reorganiseData(MultipartFile file) {
+		MultipleKnapsackModel model = utils.readMultipleKnapsacksData(file);
+		double values[] = model.getValues();
+		double weights[] = model.getWeights();
+		double binCapacities[] = model.getBinCapacities();
+		int numOfBins = model.getNumBins();
+		int numOfItems = model.getValues().length;
+
+		List<MultipleKnapsackDisplayResponse> items = new ArrayList<>();
+
+		// TODO: Create Response Item Class
+		MultipleKnapsackDataRead response = new MultipleKnapsackDataRead();
+		response.setBinCapacities(binCapacities);
+		response.setNumBins(numOfBins);
+		response.setNumItems(numOfItems);
+		for (int i = 0; i < values.length; i++) {
+			MultipleKnapsackDisplayResponse item = new MultipleKnapsackDisplayResponse();
+			item.setX(values[i]);
+			item.setY(weights[i]);
+			item.setSize(2);
+			item.setColor(1.3);
+			items.add(item);
+		}
+		response.setInitialData(items);
+		return response;
+	}
+
+	public MultipleKnapsackResponse solve(MultipartFile file) {
+		MultipleKnapsackModel data = new MultipleKnapsackModel();
+		data = utils.readMultipleKnapsacksData(file);
+		// Create the linear solver with the CBC backend.
+		MPSolver solver = new MPSolver("SimpleMipProgram",
+				MPSolver.OptimizationProblemType.CBC_MIXED_INTEGER_PROGRAMMING);
+		MPVariable[][] x = new MPVariable[data.getNumItems()][data.getNumBins()];
+		for (int i = 0; i < data.getNumItems(); ++i) {
+			for (int j = 0; j < data.getNumBins(); ++j) {
+				x[i][j] = solver.makeIntVar(0, 1, "");
+			}
+		}
+
+		for (int i = 0; i < data.getNumItems(); ++i) {
+			MPConstraint constraint = solver.makeConstraint(0, 1, "");
+			for (int j = 0; j < data.getNumBins(); ++j) {
+				constraint.setCoefficient(x[i][j], 1);
+			}
+		}
+
+		double[] binCapacities = data.getBinCapacities();
+
+		double[] weights = data.getWeights();
+		double[] values = data.getValues();
+
+		for (int j = 0; j < data.getNumBins(); ++j) {
+			MPConstraint constraint = solver.makeConstraint(0, binCapacities[j], "");
+			for (int i = 0; i < data.getNumItems(); ++i) {
+				constraint.setCoefficient(x[i][j], weights[i]);
+			}
+		}
+
+		MPObjective objective = solver.objective();
+		for (int i = 0; i < data.getNumItems(); ++i) {
+			for (int j = 0; j < data.getNumBins(); ++j) {
+				objective.setCoefficient(x[i][j], values[i]);
+			}
+		}
+		objective.setMaximization();
+
+		// MultipleKnapsackResult
+		MultipleKnapsackResponse res = new MultipleKnapsackResponse();
+
+		final MPSolver.ResultStatus resultStatus = solver.solve();
+
+		double startColor = 0.4;
+
+		// Check that the problem has an optimal solution.
+		if (resultStatus == MPSolver.ResultStatus.OPTIMAL) {
+			List<BinWrapper> bins = new ArrayList<>();
+			System.out.println("Total packed value: " + objective.value() + "\n");
+			res.setTotalPackedValue(objective.value());
+			double totalWeight = 0;
+			for (int j = 0; j < data.getNumBins(); ++j) {
+				double binWeight = 0;
+				double binValue = 0;
+				List<MultipleKnapsackDisplayResponse> points = new ArrayList<>();
+				System.out.println("Bin " + j + "\n");
+				for (int i = 0; i < data.getNumItems(); ++i) {
+					if (x[i][j].solutionValue() == 1) {
+						System.out.println("Item " + i + " - weight: " + weights[i] + "  value: " + values[i]);
+						MultipleKnapsackDisplayResponse item = new MultipleKnapsackDisplayResponse();
+						item.setColor(startColor);
+						item.setSize(2);
+						item.setX(values[i]);
+						item.setY(weights[i]);
+						item.setBin(j);
+						binWeight += weights[i];
+						binValue += values[i];
+						points.add(item);
+					}
+
+				}
+				BinWrapper bin = new BinWrapper();
+				bin.setPoints(points);
+				bin.setPackedWeight(binWeight);
+				bin.setPackedValue(binValue);
+				bins.add(bin);
+				System.out.println("Packed bin weight: " + binWeight);
+				System.out.println("Packed bin value: " + binValue + "\n");
+				totalWeight += binWeight;
+				startColor += 0.3;
+			}
+			System.out.println("Total packed weight: " + totalWeight);
+			res.setTotalPackedWeight(totalWeight);
+			res.setBins(bins);
+
+		} else {
+			System.err.println("The problem does not have an optimal solution.");
+		}
+		return res;
+	}
 }
--- src/main/java/com/or/tools/endpoints/TSPEndpoint.java
+++ src/main/java/com/or/tools/endpoints/TSPEndpoint.java
@@ -4,24 +4,15 @@ import java.util.ArrayList;
 
 import org.javatuples.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.google.ortools.constraintsolver.Assignment;
-import com.google.ortools.constraintsolver.FirstSolutionStrategy;
-import com.google.ortools.constraintsolver.RoutingIndexManager;
-import com.google.ortools.constraintsolver.RoutingModel;
-import com.google.ortools.constraintsolver.RoutingSearchParameters;
-import com.google.ortools.constraintsolver.main;
-import com.or.tools.model.DistanceMatrixModel;
-import com.or.tools.model.Row;
+import com.or.tools.algorithms.TSPService;
 import com.or.tools.requests.DistanceRequest;
 import com.or.tools.response.RoutesResponse;
 import com.or.tools.util.IOUtils;
@@ -34,15 +25,11 @@ public class TSPEndpoint {
 	private IOUtils ioUtils;
 
 	@Autowired
-	private RestTemplate rest;
-
-	private String API_key = "AIzaSyBT4mw-5ZlaiE8j_6GdNgHTgZi7PJrNa5s";
-	private static String request = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial";
+	private TSPService service;
 
 	@PostMapping("/data")
-	public Pair<long[][], ArrayList<String>> reorganiseTspData(@RequestParam("file") MultipartFile file) {
+	public Pair<long[][], ArrayList<String>> reorganiseData(@RequestParam("file") MultipartFile file) {
 		ArrayList<String> cities = ioUtils.readTspData(file);
-		cities.stream().forEach(x -> System.out.println("City: " + x));
 
 		Pair<long[][], ArrayList<String>> res = new Pair<long[][], ArrayList<String>>(distanceMatrix(cities), cities);
 
@@ -51,167 +38,12 @@ public class TSPEndpoint {
 
 	@GetMapping("/distance/matrix")
 	public long[][] distanceMatrix(ArrayList<String> cities) {
-
-		int max_elements = 100;
-		int num_addresses = cities.size();
-
-		int max_rows = max_elements / num_addresses;
-
-		int q = num_addresses / max_rows;
-		int r = num_addresses % max_rows;
-
-		System.out.println("max_row: " + max_rows + "q: " + q + "r: " + r);
-
-		DistanceMatrixModel response;
-
-		ArrayList<String> dest_addresses = new ArrayList<>();
-
-		dest_addresses = cities;
-
-		long[][] distanceMatrix = new long[cities.size()][cities.size()];
-		int x = 0;
-		int y = 0;
-
-		for (int i = 0; i < q; i++) {
-			ArrayList<String> origin_addresses = new ArrayList<>();
-			ArrayList<ArrayList<Integer>> array = new ArrayList<>();
-			for (int k = i * max_rows; k < (i + 1) * max_rows; k++) {
-				origin_addresses.add(cities.get(k));
-			}
-			response = send_request(origin_addresses, dest_addresses);
-			array = build_distance_matrix(response, cities.size());
-			for (int j = 0; j < array.size(); j++) {
-				for (int w = 0; w < array.get(0).size(); w++) {
-					distanceMatrix[x][y] = array.get(j).get(w);
-					y++;
-				}
-				y = 0;
-				x++;
-			}
-		}
-
-		if (r > 0) {
-			ArrayList<String> origin_addresses = new ArrayList<>();
-			ArrayList<ArrayList<Integer>> array = new ArrayList<>();
-			for (int i = q * max_rows; i < q * max_rows + r; i++) {
-				origin_addresses.add(cities.get(i));
-			}
-			response = send_request(origin_addresses, dest_addresses);
-			array = build_distance_matrix(response, cities.size());
-			for (int j = 0; j < array.size(); j++) {
-				for (int w = 0; w < array.get(0).size(); w++) {
-					distanceMatrix[x][y] = array.get(j).get(w);
-					y++;
-				}
-				y = 0;
-				x++;
-			}
-		}
-		return distanceMatrix;
-	}
-
-	private DistanceMatrixModel send_request(ArrayList<String> origin_addresses, ArrayList<String> dest_addresses) {
-		StringBuilder builder = new StringBuilder();
-		String origin_address_str = build_address_str(origin_addresses);
-		String dest_address_str = build_address_str(dest_addresses);
-
-		builder.append(request);
-		builder.append("&origins=");
-		builder.append(origin_address_str);
-		builder.append("&destinations=");
-		builder.append(dest_address_str);
-		builder.append("&key=");
-		builder.append(API_key);
-
-		ResponseEntity<DistanceMatrixModel> responseEntity = rest.getForEntity(builder.toString(),
-				DistanceMatrixModel.class);
-
-		DistanceMatrixModel object = responseEntity.getBody();
-		return object;
-	}
-
-	private ArrayList<ArrayList<Integer>> build_distance_matrix(DistanceMatrixModel response, int length) {
-		ArrayList<ArrayList<Integer>> distanceMatrix = new ArrayList<>();
-		// long[][] distances = new long[length][length];
-		ArrayList<Row> rows = response.getRows();
-		for (int i = 0; i < rows.size(); i++) {
-			ArrayList<Integer> row = new ArrayList<Integer>();
-			for (int j = 0; j < rows.get(i).getElements().size(); j++) {
-				row.add(rows.get(i).getElements().get(j).getDistance().getValue() / 1000);
-				// distances[i][j] = (long) ((Integer
-				// .toUnsignedLong(rows.get(i).getElements().get(j).getDistance().getValue()) /
-				// 1000) * 1.609344);
-			}
-			distanceMatrix.add(row);
-		}
-		return distanceMatrix;
-	}
-
-	private String build_address_str(ArrayList<String> addresses) {
-		String address_str = "";
-		StringBuilder builder = new StringBuilder();
-		builder.append(address_str);
-		for (int i = 0; i < addresses.size(); i++) {
-			builder.append(addresses.get(i));
-			builder.append("|");
-		}
-		builder.deleteCharAt(builder.length() - 1);
-		return builder.toString();
+		return service.calculateDistanceMatrix(cities);
 	}
 
 	@PostMapping("/result")
 	public RoutesResponse tspResult(@RequestBody DistanceRequest request) {
-		// Instantiate the data problem.
-
-		// Create Routing Index Manager
-		RoutingIndexManager manager = new RoutingIndexManager(request.getDistances().length, 1, 0);
-
-		// Create Routing Model.
-		RoutingModel routing = new RoutingModel(manager);
-
-		// Create and register a transit callback.
-		final int transitCallbackIndex = routing.registerTransitCallback((long fromIndex, long toIndex) -> {
-			// Convert from routing variable Index to user NodeIndex.
-			int fromNode = manager.indexToNode(fromIndex);
-			int toNode = manager.indexToNode(toIndex);
-			long[][] distanceMatrix = request.getDistances();
-			return distanceMatrix[fromNode][toNode];
-		});
-
-		// Define cost of each arc.
-		routing.setArcCostEvaluatorOfAllVehicles(transitCallbackIndex);
-
-		// Setting first solution heuristic.
-		RoutingSearchParameters searchParameters = main.defaultRoutingSearchParameters().toBuilder()
-				.setFirstSolutionStrategy(FirstSolutionStrategy.Value.PATH_CHEAPEST_ARC).build();
-
-		// Solve the problem.
-		Assignment solution = routing.solveWithParameters(searchParameters);
-
-		// Solution cost.
-		System.out.println("Objective: " + solution.objectiveValue() + "miles");
-		// Inspect solution.
-		System.out.println("Route:");
-		long routeDistance = 0;
-		String route = "";
-		long index = routing.start(0);
-		ArrayList<Integer> routes = new ArrayList<>();
-		while (!routing.isEnd(index)) {
-			route += manager.indexToNode(index) + " -> ";
-			routes.add(manager.indexToNode(index));
-			long previousIndex = index;
-			index = solution.value(routing.nextVar(index));
-			routeDistance += routing.getArcCostForVehicle(previousIndex, index, 0);
-		}
-		route += manager.indexToNode(routing.end(0));
-		System.out.println(route);
-		System.out.println("Route distance: " + routeDistance + "miles");
-		routes.forEach(r -> System.out.println("Route " + r));
-
-		RoutesResponse response = new RoutesResponse();
-		response.setRoutes(routes);
-		response.setTotalDistance(routeDistance);
-		return response;
+		return service.solve(request);
 	}
 
 }
