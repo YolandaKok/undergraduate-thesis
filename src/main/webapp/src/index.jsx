c2991e17ec109568a348fd50d3fda2d6377943f1
YolandaKok <yolanda.kokkinou@gmail.com>
2020-03-03 23:34:46 +0200

    token decode

--- src/main/webapp/src/index.jsx
+++ src/main/webapp/src/index.jsx
@@ -19,61 +19,38 @@ import LinearOpt from "./components/algorithms/LinearOpt";
 import ShowResultLinear from "./components/layout/ShowResultLinear";
 import NotFound from "./components/pages/NotFound";
 
+const jwtDecode = require('jwt-decode');
+
 const routing = (
     <BrowserRouter>
         <Switch>
-            <Route exact path="/signin" component={() => ["null", '', undefined, null].every((text) => {
-                return localStorage.getItem('authorization') !== text;
-            }) ? <Homepage><HomepageLayout/></Homepage> : <SignIn/>}/>
-            <Route exact path="/signup" component={() => ["null", '', undefined, null].every((text) => {
-                return localStorage.getItem('authorization') !== text;
-            }) ? <Homepage><HomepageLayout/></Homepage> : <SignUp/>}/>
-            <Route exact path="/" component={() => ["null", '', undefined, null].every((text) => {
-                return localStorage.getItem('authorization') !== text;
-            }) ? <Homepage><HomepageLayout/></Homepage> : <IndexPage/>}/>
-            <Route exact path="/select/algorithm" component={() => ["null", '', undefined, null].every((text) => {
-                return localStorage.getItem('authorization') !== text;
-            }) ? <Homepage><SelectAlgorithm/></Homepage> : <IndexPage/>}/>
+            <Route exact path="/signin" component={() => checkToken() ? <Homepage><HomepageLayout/></Homepage> : <SignIn/>}/>
 
-            <Route exact path="/algorithms/knapsack/:id" component={() => ["null", '', undefined, null].every((text) => {
-                return localStorage.getItem('authorization') !== text;
-            }) ? <Homepage><Knapsack/></Homepage> : <IndexPage/>}></Route>
+            <Route exact path="/signup" component={() => checkToken() ? <Homepage><HomepageLayout/></Homepage> : <SignUp/>}/>
 
-            <Route exact path="/algorithms/multipleknapsacks/:id" component={() => ["null", '', undefined, null].every((text) => {
-                return localStorage.getItem('authorization') !== text;
-            }) ? <Homepage><MultipleKnapsacks/></Homepage> : <IndexPage/>}></Route>
+            <Route exact path="/" component={() => checkToken() ? <Homepage><HomepageLayout/></Homepage> : <IndexPage/>}/>
 
-            <Route exact path="/algorithms/travellingsalesman/:id" component={() => ["null", '', undefined, null].every((text) => {
-                return localStorage.getItem('authorization') !== text;
-            }) ? <Homepage><TSP/></Homepage> : <IndexPage/>}></Route>
+            <Route exact path="/select/algorithm" component={() => checkToken() ? <Homepage><SelectAlgorithm/></Homepage> : <IndexPage/>}/>
 
-            <Route exact path="/algorithms/linearoptimization/:id" component={() => ["null", '', undefined, null].every((text) => {
-                return localStorage.getItem('authorization') !== text;
-            }) ? <Homepage><LinearOpt/></Homepage> : <IndexPage/>}/>
+            <Route exact path="/algorithms/knapsack/:id" component={() => checkToken() ? <Homepage><Knapsack/></Homepage> : <IndexPage/>}></Route>
 
-            <Route exact path="/myexperiments" component={() => ["null", '', undefined, null].every((text) => {
-                return localStorage.getItem('authorization') !== text;
-            }) ? <Homepage><MyExperiments/></Homepage> : <IndexPage/>}></Route>
+            <Route exact path="/algorithms/multipleknapsacks/:id" component={() => checkToken() ? <Homepage><MultipleKnapsacks/></Homepage> : <IndexPage/>}></Route>
 
-            <Route exact path="/profile" component={() => ["null", '', undefined, null].every((text) => {
-                return localStorage.getItem('authorization') !== text;
-            }) ? <Homepage><ManageProfile/></Homepage> : <IndexPage/>}/>
+            <Route exact path="/algorithms/travellingsalesman/:id" component={() => checkToken() ? <Homepage><TSP/></Homepage> : <IndexPage/>}></Route>
 
-            <Route exact path="/show/result/knapsack/:id" component={() => ["null", '', undefined, null].every((text) => {
-            return localStorage.getItem('authorization') !== text;
-            }) ? <Homepage><ShowResult/></Homepage> : <IndexPage/>}/>
+            <Route exact path="/algorithms/linearoptimization/:id" component={() => checkToken() ? <Homepage><LinearOpt/></Homepage> : <IndexPage/>}/>
 
-            <Route exact path="/show/result/multipleknapsacks/:id" component={() => ["null", '', undefined, null].every((text) => {
-                return localStorage.getItem('authorization') !== text;
-            }) ? <Homepage><ShowResultMultiple/></Homepage> : <IndexPage/>}/>
+            <Route exact path="/myexperiments" component={() => checkToken() ? <Homepage><MyExperiments/></Homepage> : <IndexPage/>}></Route>
 
-            <Route exact path="/show/result/travellingsalesman/:id" component={() => ["null", '', undefined, null].every((text) => {
-                return localStorage.getItem('authorization') !== text;
-            }) ? <Homepage><ShowResultRouting/></Homepage> : <IndexPage/>}/>
+            <Route exact path="/profile" component={() => checkToken() ? <Homepage><ManageProfile/></Homepage> : <IndexPage/>}/>
 
-            <Route exact path="/show/result/linearoptimization/:id" component={() => ["null", '', undefined, null].every((text) => {
-                return localStorage.getItem('authorization') !== text;
-            }) ? <Homepage><ShowResultLinear/></Homepage> : <IndexPage/>}/>
+            <Route exact path="/show/result/knapsack/:id" component={() => checkToken() ? <Homepage><ShowResult/></Homepage> : <IndexPage/>}/>
+
+            <Route exact path="/show/result/multipleknapsacks/:id" component={() => checkToken() ? <Homepage><ShowResultMultiple/></Homepage> : <IndexPage/>}/>
+
+            <Route exact path="/show/result/travellingsalesman/:id" component={() => checkToken() ? <Homepage><ShowResultRouting/></Homepage> : <IndexPage/>}/>
+
+            <Route exact path="/show/result/linearoptimization/:id" component={() => checkToken() ? <Homepage><ShowResultLinear/></Homepage> : <IndexPage/>}/>
 
             <Route path="">
                 <Homepage><NotFound/></Homepage>
@@ -81,6 +58,15 @@ const routing = (
         </Switch>
     </BrowserRouter>
 );
+
+function checkToken() {
+    try {
+        let decoded = jwtDecode(localStorage.getItem('authorization').substring(7,localStorage.getItem('authorization').length));
+        return true;
+    } catch(err) {
+        return false;
+    }
+}
 
 ReactDOM.render(
     routing, document.getElementById('app')
